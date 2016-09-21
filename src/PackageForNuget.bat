f934ed660471ac8be974f4fc564626fe7d73bfe9
Cori Drew <Cori.Drew@improving.com>
2016-09-21 10:36:08 -0500

    changed build output path so debug build doesn't break

--- src/PackageForNuget.bat
+++ src/PackageForNuget.bat
@@ -6,15 +6,15 @@ echo Updating NuGet
 .\.nuget\nuget.exe Update -self
 
 echo Building library package
-.\.nuget\nuget.exe pack .\NuGetPackage-Lib\bin\release\NRConfig.Library.nuspec -OutputDirectory .\NuGetPackage-Lib\bin\release\
-copy .\NuGetPackage-Lib\bin\release\NRConfig.Library.*.nupkg ..\Packaged\
+.\.nuget\nuget.exe pack .\NuGetPackage-Lib\bin\NRConfig.Library.nuspec -OutputDirectory .\NuGetPackage-Lib\bin\
+copy .\NuGetPackage-Lib\bin\NRConfig.Library.*.nupkg ..\Packaged\
 
 echo Building tool package
-.\.nuget\nuget.exe pack .\NuGetPackage-Tool\bin\release\NRConfig.Tool.nuspec -OutputDirectory .\NuGetPackage-Tool\bin\release\
-copy .\NuGetPackage-Tool\bin\release\NRConfig.Tool.*.nupkg ..\Packaged\
+.\.nuget\nuget.exe pack .\NuGetPackage-Tool\bin\NRConfig.Tool.nuspec -OutputDirectory .\NuGetPackage-Tool\bin\
+copy .\NuGetPackage-Tool\bin\NRConfig.Tool.*.nupkg ..\Packaged\
 
 echo Building MSBuild package
-.\.nuget\nuget.exe pack .\NuGetPackage-MSBuild\bin\release\NRConfig.MSBuild.nuspec -OutputDirectory .\NuGetPackage-MSBuild\bin\release\
-copy .\NuGetPackage-MSBuild\bin\release\NRConfig.MSBuild.*.nupkg ..\Packaged\
+.\.nuget\nuget.exe pack .\NuGetPackage-MSBuild\bin\NRConfig.MSBuild.nuspec -OutputDirectory .\NuGetPackage-MSBuild\bin\
+copy .\NuGetPackage-MSBuild\bin\NRConfig.MSBuild.*.nupkg ..\Packaged\
 
 echo Packaging complete.
